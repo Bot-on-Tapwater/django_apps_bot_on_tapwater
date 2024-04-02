4289b35c2fa7e22c8e326f22f3d6a11f7807b2bb
raymond <stancillousray@gmail.com>
2024-04-02 12:47:45 +0300

    Set name field of Category to be unique.

--- eridosolutions/models.py
+++ eridosolutions/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
